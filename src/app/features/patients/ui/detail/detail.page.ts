5624fbc3ed2e642ffba7b2e004891a2ad7d232bd
upcteus <u202210059@upc.edu.pe>
2025-10-08 20:37:24 -0500

    a

--- src/app/features/patients/ui/detail/detail.page.ts
+++ src/app/features/patients/ui/detail/detail.page.ts
@@ -18,27 +18,26 @@ import {ComplaintsComponent} from "./widgets/complaints/complaints.component";
     selector: 'app-detail-page',
     template: `
         <div class="row">
-        <div class="col-lg-12 col-xxxl-12">
-            <!-- Welcome -->
-            <app-welcome />
-        </div>
-        <div class="col-lg-12 col-xxxl-12">
-            <div class="row">
-                <div class="col-md-4">
-                    <!-- Active Courses -->
-                    <app-active-courses />
-                </div>
-                <div class="col-md-4">
-                    <!-- Enrolled Students -->
-                    <app-enrolled-students />
-                </div>
-                <div class="col-md-4">
-                    <!-- Completion Status -->
-                    <app-completion-status />
+            <div class="col-lg-12 col-xxxl-12">
+                <!-- Welcome -->
+                <app-welcome />
+            </div>
+            <div class="col-lg-12 col-xxxl-12">
+                <div class="row">
+                    <div class="col-md-4">
+                        <!-- Active Courses -->
+                        <app-active-courses />
+                    </div>
+                    <div class="col-md-4">
+                        <!-- Enrolled Students -->
+                        <app-enrolled-students />
+                    </div>
+                    <div class="col-md-4">
+                        <!-- Completion Status -->
+                        <app-completion-status />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
             <div class="col-lg-6 col-xxxl-4">
                 <!-- Semi Circular Gauge RadialBar Chart -->
                 <app-semi-circular-gauge-radialbar-chart />
@@ -51,15 +50,11 @@ import {ComplaintsComponent} from "./widgets/complaints/complaints.component";
                 <!-- Basic Pie Chart -->
                 <app-pie-chart />
             </div>
-        </div>
-        <div class="row">
             <div class="col-lg-6 col-xxxl-12">
                 <!-- Tasks Stats -->
                 <app-tasks-stats />   
             </div>
-        </div>
-        <app-timeline/>
-        <div class="row">
+            <app-timeline/>
             <div class="col-md-6 col-xxxl-6">
                 <!-- First Response Time -->
                 <app-first-response-time />
@@ -73,7 +68,6 @@ import {ComplaintsComponent} from "./widgets/complaints/complaints.component";
                 <app-complaints />
             </div>
         </div>
-    </div>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [
