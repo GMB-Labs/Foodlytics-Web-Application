7ec5dccdc11f2f5bc2392d82f537ea10690ca6db
teus te mete el chop <u202210059@upc.edu.pe>
2025-10-08 19:30:01 -0500

    hola

--- src/app/features/patients/routes.ts
+++ src/app/features/patients/routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { PatientsFeature } from './feature.component';
+import {DetailPage} from "./ui/detail/detail.page";
 
 export const PATIENTS_ROUTES: Routes = [
     {
@@ -9,7 +10,7 @@ export const PATIENTS_ROUTES: Routes = [
             { path: '', loadComponent: () => import('./ui/list/patients-list.component').then(m => m.PatientsListComponent) },
             { path: 'create', loadComponent: () => import ('./ui/create/create-patient.component').then(m => m.CreatePatientComponent) },
             { path: 'edit/:id', loadComponent: () => import ('./ui/edit/edit-patient.component').then(m => m.EditPatientComponent) },
-            { path: 'overview/:id', loadComponent:() => import('./ui/detail/detail.page').then(m => m.DetailPage)},
+            { path: 'overview/:id', component: DetailPage},
         ]
     }
 ];
