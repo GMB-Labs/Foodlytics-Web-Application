fce65f0750c84c0cb6f7ba0d06aa43544e98b257
teus te mete el chop <u202210059@upc.edu.pe>
2025-10-29 22:12:04 -0500

    feat(pwa): add PWA support
    Adds Progressive Web App (PWA) capabilities to the project using `ng add @angular/pwa@20.3.8`.
    This automatically configures the project with a service worker, a `manifest.webmanifest` file, and updates the necessary module and HTML files to enable offline support.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {
     ApplicationConfig, provideBrowserGlobalErrorListeners,
-    provideZonelessChangeDetection
+    provideZonelessChangeDetection, isDevMode
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
@@ -9,6 +9,7 @@ import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi}
 import {provideAnimations} from "@angular/platform-browser/animations";
 import {provideBreadcrumbsFromRouter} from "./shared/data-access/breadcrumb/breadcrumb.providers";
 import {AuthHttpInterceptor, provideAuth0} from "@auth0/auth0-angular";
+import { provideServiceWorker } from '@angular/service-worker';
 
 const isBrowser = typeof window !== 'undefined';
 
@@ -31,6 +32,9 @@ export const appConfig: ApplicationConfig = {
         provideClientHydration(withIncrementalHydration()),
         provideZonelessChangeDetection(),
         provideBreadcrumbsFromRouter(),
-        provideAnimations()
+        provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
     ]
 };
