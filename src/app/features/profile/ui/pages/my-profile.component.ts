abe1858aa86521477f8c08acd66fd2a8d14c0217
teus te mete el chop <113954621+teuschop@users.noreply.github.com>
2025-11-13 02:09:10 -0500

    refactor(profile): remove unused profile components from template and imports

--- src/app/features/profile/ui/pages/my-profile.component.ts
+++ src/app/features/profile/ui/pages/my-profile.component.ts
@@ -3,16 +3,14 @@ import { WelcomeComponent } from '../components/welcome/welcome.component';
 import { TotalProjectsComponent } from '../components/total-projects/total-projects.component';
 import { TotalRevenueComponent } from '../components/total-revenue/total-revenue.component';
 import { TotalOrdersComponent } from '../components/total-orders/total-orders.component';
-import { ProfileIntroComponent } from '../components/profile-intro/profile-intro.component';
 import { ProfileInformationComponent } from '../components/profile-information/profile-information.component';
-import { AdditionalInformationComponent } from '../components/additional-information/additional-information.component';
 import { OverviewComponent } from '../components/overview/overview.component';
 import { ToDoListComponent } from '../components/to-do-list/to-do-list.component';
 import { RecentActivityComponent } from '../components/recent-activity/recent-activity.component';
 
 @Component({
     selector: 'app-profile',
-    imports: [WelcomeComponent, TotalProjectsComponent, TotalOrdersComponent, TotalRevenueComponent, ProfileIntroComponent, ProfileInformationComponent, AdditionalInformationComponent, OverviewComponent, ToDoListComponent, RecentActivityComponent],
+    imports: [WelcomeComponent, TotalProjectsComponent, TotalOrdersComponent, TotalRevenueComponent, ProfileInformationComponent, OverviewComponent, ToDoListComponent, RecentActivityComponent],
     templateUrl: './my-profile.component.html',
     styleUrl: './my-profile.component.scss'
 })
